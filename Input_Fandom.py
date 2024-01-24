b247a80c5ab86ac88fee1707429715ee0e2f101a
DeRecnad <gamer_red@mail.ru>
2024-01-24 16:02:17 +0500

    Попытался сделать один логин для всех взаимодействий

--- Input_Fandom.py
+++ Input_Fandom.py
@@ -8,100 +8,110 @@ from selenium.common.exceptions import NoSuchElementException
 import time
 import pyperclip
 
+import time
 
-def edit_and_save_text(url, filename, username, password):
-    # Инициализация браузера
-    service = webdriver.chrome.service.Service(
-        executable_path="chromedriver.exe")
 
-    # Опции браузера
-    options = webdriver.ChromeOptions()
-    options.add_argument('--disable-blink-features=AutomationControlled')
-    #options.add_argument("--headless=new")
-    options.set_capability('pageLoadStrategy', "eager")
+def close_browser(driver):
+    # Закрытие браузера
+    driver.close()
+    driver.quit()
 
-    with webdriver.Chrome(service=service, options=options) as driver:
+def login(username, password, driver):
+    try:
+        # Открытие страницы фандома
+        driver.get('https://auth.fandom.com/signin?flow=7b6a4faa-9098-4b27-b6b7-01ad0d605e26')
+        time.sleep(2)
+
+        # Находим поля для ввода логина и пароля
+        login_field = driver.find_element(by=By.ID, value="identifier")
+        password_field = driver.find_element(by=By.ID, value="password")
+
+        # Вводим логин и пароль
+        login_field.send_keys(username)
+        print("Введено имя")
+        password_field.send_keys(password)
+        print("Введён пароль")
+        time.sleep(2)
+        # Находим кнопку "Войти"
+        login_button = driver.find_element(by=By.XPATH, value="//button[@class='wds-button Submit_button__2u2G7']")
+
+        # Кликаем по кнопке "Войти"
+        login_button.click()
+        print('Нажата кнопка "Войти"')
+    except MaxRetryError as ex:
+        print(f"MaxRetryError: {ex}")
+    except Exception as ex:
+        print(ex)
+    finally:
+        close_browser(driver)
+
+
+def edit_and_save_text(url, filename, username, password, driver):
+    try:
+        # Переходим по ссылке
+        driver.get(url)
+        print('Выполнен переход на страницу')
+        time.sleep(5)
         try:
-            start = time.time()
-            # Открытие страницы фандома
-            driver.get('https://auth.fandom.com/signin?flow=7b6a4faa-9098-4b27-b6b7-01ad0d605e26')
+            # Найти элемент по комбинированному селектору
+            enter_button = driver.find_element(by=By.XPATH, value="//span[@class='oo-ui-widget oo-ui-widget-enabled oo-ui-buttonElement oo-ui-buttonElement-framed oo-ui-labelElement oo-ui-flaggedElement-primary oo-ui-flaggedElement-progressive oo-ui-buttonWidget oo-ui-actionWidget']")
 
-            # Находим поля для ввода логина и пароля
-            login_field = driver.find_element(by=By.ID, value="identifier")
-            password_field = driver.find_element(by=By.ID, value="password")
+            # Нажать на кнопку
+            enter_button.click()
+            print('Кнопка "Сохранить" нажата')
+        except Exception as e:
+            print(f'Ошибка при нажатии кнопки "Сохранить": {e}')
+            print('Кнопка не была найдена')
 
-            # Вводим логин и пароль
-            login_field.send_keys(username)
-            print("Введено имя")
-            password_field.send_keys(password)
-            print("Введён пароль")
-
-            # coockies
-            pickle.dump(driver.get_cookies(), open(f"{username}_cookies", "wb"))
-
-            # Находим кнопку "Войти"
-            login_button = driver.find_element(by=By.XPATH, value="//button[@class='wds-button Submit_button__2u2G7']")
-
-            # Кликаем по кнопке "Войти"
-            login_button.click()
-            print('Нажата кнопка "Войти"')
-
-            # Переходим по ссылке
-            driver.get(url)
-            print('Выполнен переход на страницу')
-            time.sleep(5)
-            try:
-                # Найти элемент по комбинированному селектору
-                enter_button = driver.find_element(by=By.XPATH, value="//span[@class='oo-ui-widget oo-ui-widget-enabled oo-ui-buttonElement oo-ui-buttonElement-framed oo-ui-labelElement oo-ui-flaggedElement-primary oo-ui-flaggedElement-progressive oo-ui-buttonWidget oo-ui-actionWidget']")
-
-                # Нажать на кнопку
-                enter_button.click()
-                print('Кнопка "Сохранить" нажата')
-            except Exception as e:
-                print(f'Ошибка при нажатии кнопки "Сохранить": {e}')
-                print('Кнопка не была найдена')
-
-            # Получаем редактируемый элемент
-            editable_element = driver.find_element(by=By.XPATH,
+        # Получаем редактируемый элемент
+        editable_element = driver.find_element(by=By.XPATH,
                                                    value="//div[@class='ve-ce-branchNode ve-ce-documentNode ve-ce-attachedRootNode ve-ce-rootNode mw-content-ltr mw-parser-output mw-show-empty-elt ve-ce-documentNode-codeEditor-webkit-hide']")
 
-            # Получаем вложенный параграф
-            paragraph_element = editable_element.find_element(by=By.XPATH,
+        # Получаем вложенный параграф
+        paragraph_element = editable_element.find_element(by=By.XPATH,
                                                               value=".//p[@class='ve-ce-branchNode ve-ce-contentBranchNode ve-ce-paragraphNode']")
 
-            # Читаем текст из файла
-            with open(filename, 'r', encoding='utf-8') as f:
-                new_text = f.read()
-            print(f"Текст получен")
-            time.sleep(1)
+        # Читаем текст из файла
+        with open(filename, 'r', encoding='utf-8') as f:
+            new_text = f.read()
+        print(f"Текст получен")
+        time.sleep(1)
 
-            # Очищаем и вводим новый текст
-            paragraph_element.send_keys(Keys.CONTROL + "a")
-            time.sleep(1)
-            paragraph_element.send_keys(Keys.DELETE)
+        # Очищаем и вводим новый текст
+        paragraph_element.send_keys(Keys.CONTROL + "a")
+        time.sleep(1)
+        paragraph_element.send_keys(Keys.DELETE)
 
-            print("Очищено, приступаем писать текст")
-            for line in new_text.splitlines():
-                paragraph_element.send_keys(line)
-                paragraph_element.send_keys(Keys.ENTER)
-                time.sleep(0.1)
-            print("Написано")
+        print("Очищено, приступаем писать текст")
+        for line in new_text.splitlines():
+            paragraph_element.send_keys(line)
+            paragraph_element.send_keys(Keys.ENTER)
+            time.sleep(0.1)
+        print("Написано")
 
-            # Находим кнопку сохранения по тексту внутри кнопки
-            save_button = driver.find_element(by=By.XPATH, value="//button[contains(.,'Сохранить')]")
-            # Кликаем по кнопке сохранения
-            save_button.click()
-            time.sleep(2)
-            print("Сохранено")
-            end = time.time() - start
-            print(f"Время выполнения: {end} сек.")
+        # Находим кнопку сохранения по тексту внутри кнопки
+        save_button = driver.find_element(by=By.XPATH, value="//button[contains(.,'Сохранить')]")
+        # Кликаем по кнопке сохранения
+        save_button.click()
+        time.sleep(2)
+        print("Сохранено")
 
+<<<<<<< Updated upstream
             driver.close()
             driver.quit()
         except MaxRetryError as ex:
             print(f"MaxRetryError: {ex}")
         except Exception as ex:
             print(ex)
+=======
+    except MaxRetryError as ex:
+        print(f"MaxRetryError: {ex}")
+    except Exception as ex:
+        print(ex)
+    finally:
+        close_browser(driver)
+
+>>>>>>> Stashed changes
 
 
 # Пример использования в другой программе:
--- logic.py
+++ logic.py
@@ -1,7 +1,38 @@
 from Copy_Corvax import get_specific_text, write_data_to_file
-from Input_Fandom import edit_and_save_text
 from proxy_auth_data import username, password
+<<<<<<< Updated upstream
 import traceback
+=======
+from Input_Fandom import login, edit_and_save_text
+
+import pickle
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from urllib3.exceptions import MaxRetryError
+from selenium.common.exceptions import NoSuchElementException
+
+import time
+import pyperclip
+
+def initialize_browser():
+    # Инициализация браузера
+    service = webdriver.chrome.service.Service(executable_path="chromedriver.exe")
+
+    # Опции браузера
+    options = webdriver.ChromeOptions()
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    # options.add_argument("--headless=new")
+    options.set_capability('pageLoadStrategy', "eager")
+
+    return webdriver.Chrome(service=service, options=options)
+
+
+def close_browser(driver):
+    # Закрытие браузера
+    driver.close()
+    driver.quit()
+>>>>>>> Stashed changes
 
 
 def check_links(entry1_value, entry2_value, setting_vars):
@@ -62,7 +93,7 @@ def rename_links(entry1_value, entry2_value, setting_vars):
     return link1, link2
 
 
-def process_links(link1, link2):
+def process_links(link1, link2, driver):
     filename = 'filename.txt'
     print("Получаем текст")
     if get_specific_text(link1) == 'ERR520':
@@ -72,14 +103,20 @@ def process_links(link1, link2):
 
     # Обновляем текст на второй Вики-странице
     print("Начинаем вводить текст")
-    edit_and_save_text(link2, filename, username, password)
+    edit_and_save_text(link2, filename, username, password, driver)
     print("Ввод окончен")
 
+<<<<<<< Updated upstream
 
 def copy_and_update_links(link1, link2):
     process_links(link1, link2)
+=======
+def copy_and_update_links(link1, link2, driver):
+    process_links(link1, link2, driver)
+>>>>>>> Stashed changes
 
 
+<<<<<<< Updated upstream
 def check_count_links(link1, link2, setting_vars):
     count_templates_links = write_data_to_file(link1)
     if setting_vars.use_templates:
@@ -107,3 +144,24 @@ def check_count_links(link1, link2, setting_vars):
     else:
         print(f'Неизвестная ошибка')
 
+=======
+    if count_templates_links == 'E520':
+        print('Ошибка 520')
+    elif count_templates_links > 0:
+        initialize_browser()
+        with initialize_browser() as driver:
+            main1_link = link1
+            main2_link = link2
+            login(username, password, driver)
+            while count_templates_links != 0:
+                link2 = get_and_remove_last_link()
+                link1 = link2
+                link1, link2 = rename_links(link1, link2, use_single_entry)
+                process_links(link1, link2, driver)
+                count_templates_links -= 1
+            process_links(main1_link, main2_link, driver)
+        return
+    else:
+        with initialize_browser() as driver:
+            copy_and_update_links(link1, link2, driver)
+>>>>>>> Stashed changes
